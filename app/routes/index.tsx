0ff54a0cbd450002a0a7549c665721cace989840
connergillette <connerwgillette@gmail.com>
2023-01-13 20:06:27 -0800

    Fix gradients / scroll area spacing

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,16 +19,16 @@ export default function Index() {
           <h3 className="font-['Bely'] mt-3 text-2xl">Conner Gillette</h3>
         </div>
         <div className="flex flex-col w-[60%] h-screen items-center content-center justify-center align-center gap-10 ml-auto">
-          <div className="h-24 w-full fixed top-0 bg-gradient-to-t from-transparent to-white"></div>
-          <div className="flex flex-col gap-3 snap-y snap-mandatory ml-auto w-10/12 pb-[400px] pt-[100%]">
+          <div className="h-24 w-full fixed top-0 bg-gradient-to-t from-transparent to-white z-10"></div>
+          <div className="flex flex-col gap-3 snap-y snap-mandatory ml-auto w-10/12 pb-[400px] pt-[170%]">
             {
               projects && projects.map((project) => (
-                <ProjectTile 
-                  key={project.id} 
-                  month={project.label} 
-                  bg={`bg-[${project.theme_color}]`} 
-                  text={`text-[${project.theme_color}]`} 
-                  projectName={project.name?.toUpperCase() || '???'} 
+                <ProjectTile
+                  key={project.id}
+                  month={project.label}
+                  bg={`bg-[${project.theme_color}]`}
+                  text={`text-[${project.theme_color}]`}
+                  projectName={project.name?.toUpperCase() || '???'}
                   progress={project.progress}
                   next={project.is_next}
                 />
@@ -38,7 +38,7 @@ export default function Index() {
               !projects && "Projects coming soon!"
             }
           </div>
-          <div className="h-24 w-full fixed bottom-0 bg-gradient-to-t from-white to-transparent"></div>
+          <div className="h-24 w-full fixed bottom-0 bg-gradient-to-t from-white to-transparent z-10"></div>
         </div>
       </div>
     </div>
