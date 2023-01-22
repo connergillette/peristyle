80614c6454b44e07f318ed6f8f40e91b4e3b6c5c
connergillette <connerwgillette@gmail.com>
2023-01-21 18:30:32 -0800

    Fix weird rounding in UpdateOverview

--- app/components/UpdateOverview.tsx
+++ app/components/UpdateOverview.tsx
@@ -6,8 +6,8 @@ export default function UpdateOverview ({ update, slug }: { update: Update, slug
     <Link to={slug}>
       <div className="flex flex-row rounded-md">
         <div className="pr-3 pt-1">
-          <div className="h-16 w-16 border-solid border-2 border-gray-400 rounded-md">
-            <img className="h-full w-full object-cover rounded-md" src={update.main_image_url} alt={update.title} />
+          <div className="h-16 w-16">
+            <img className="h-full w-full object-cover border-solid border-2 border-gray-400 rounded-md" src={update.main_image_url} alt={update.title} />
           </div>
         </div>
         <div>
