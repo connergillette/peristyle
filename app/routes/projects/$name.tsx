53c535848fff0148efd2b73e58dcf8df2c26d437
connergillette <connerwgillette@gmail.com>
2023-01-23 22:02:23 -0800

    Update Coming Soon views

--- app/routes/projects/$name.tsx
+++ app/routes/projects/$name.tsx
@@ -30,47 +30,58 @@ export default function ProjectName () {
             {project?.description || "Coming soon"}
           </div>
         </div>
-        <div className="flex h-full overflow-hidden text-[#333333] mb-5 gap-5">
-          <div className="flex flex-col w-full rounded-md">
-            <div className="flex flex-row max-xl:flex-col gap-5 justify-center">
-              {
-                project?.image_url && (
-                  <Link to="/" className="w-8/12 max-xl:w-full">
+        {
+          !project.details && (
+            <div className="flex w-full h-full content-center items-center align-center justify-center mt-[10%] max-xl:mt-[20%]">
+              <h2 className="flex text-center text-[42px] max-xl:text-2xl opacity-50">Coming soon.</h2>
+            </div>
+          )
+        }
+        {
+          project.details && (
+            <div className="flex h-full overflow-hidden text-[#333333] mb-5 gap-5">
+              <div className="flex flex-col w-full rounded-md">
+                <div className="flex flex-row max-xl:flex-col gap-5 justify-center">
+                  {
+                    project?.image_url && (
+                      <Link to="/" className="w-8/12 max-xl:w-full">
+                        {
+                          <img src={project?.image_url} alt={`${project.name} home screen`} className="w-full h-auto rounded-md" />
+                        }
+                      </Link>
+                    )
+                  }
+                    
+                  <div className="flex flex-col font-['bely-display'] relative bg-white rounded-md max-xl:w-full w-4/12 min-h-[400px]">
+                    <div className="p-5">
+                      <h2 className="text-2xl mb-3">Updates</h2>
+                      <div className="flex flex-col grow gap-3 scroll-smooth w-full h-full rounded-md overflow-y-auto max-h-[600px] pb-[100px]">
+                        {
+                          updates.map((update: Update) => update && <UpdateOverview key={update.id} slug={`/projects/${name}/${update.slug}`} update={update} /> )
+                        }
+                      </div>
+                    </div>
+                    {/* TODO: Potentially unused */}
+                    {/* <div className="flex justify-center align-center">
+                      <button className="button bg-[#495993] text-white w-min p-2 mt-2 rounded-md">More</button>
+                    </div> */}
+                    <div className="h-32 w-full absolute bottom-0 bg-gradient-to-t from-white to-transparent select-none pointer-events-none rounded-md"></div>
+                  </div>
+                </div>
+                <div className="text-white w-8/12 max-xl:w-full">
+                  { project.details && 
+                    <article className="p-5 pt-10 prose-lg prose-invert" dangerouslySetInnerHTML={{__html: md.render(project.details)}}/>
+                  }
+                  <div className="flex flex-row text-xl gap-x-1 gap-y-3 align-center mt-12 font-['bely-display'] p-5 w-full flex-wrap justify-evenly">
                     {
-                      <img src={project?.image_url} alt={`${project.name} home screen`} className="w-full h-auto rounded-md" />
-                    }
-                  </Link>
-                )
-              }
-                
-              <div className="flex flex-col font-['bely-display'] relative bg-white rounded-md max-xl:w-full w-4/12 min-h-[400px]">
-                <div className="p-5">
-                  <h2 className="text-2xl mb-3">Updates</h2>
-                  <div className="flex flex-col grow gap-3 scroll-smooth w-full h-full rounded-md overflow-y-auto max-h-[600px] pb-[100px]">
-                    {
-                      updates.map((update: Update) => update && <UpdateOverview key={update.id} slug={`/projects/${name}/${update.slug}`} update={update} /> )
+                      project.tech_items && project.tech_items.split(',').map(techItem => <div key={techItem.trim()} className="flex rounded-md bg-white bg-opacity-30 px-5 py-3 transition hover:scale-105">{techItem.trim()}</div>)
                     }
                   </div>
                 </div>
-                {/* TODO: Potentially unused */}
-                {/* <div className="flex justify-center align-center">
-                  <button className="button bg-[#495993] text-white w-min p-2 mt-2 rounded-md">More</button>
-                </div> */}
-                <div className="h-32 w-full absolute bottom-0 bg-gradient-to-t from-white to-transparent select-none pointer-events-none rounded-md"></div>
               </div>
             </div>
-            <div className="text-white w-8/12 max-xl:w-full">
-              { project.details && 
-                <article className="p-5 pt-10 prose-lg prose-invert" dangerouslySetInnerHTML={{__html: md.render(project.details)}}/>
-              }
-              <div className="flex flex-row text-xl gap-x-1 gap-y-3 align-center mt-12 font-['bely-display'] p-5 w-full flex-wrap justify-evenly">
-                {
-                  project.tech_items && project.tech_items.split(',').map(techItem => <div key={techItem.trim()} className="flex rounded-md bg-white bg-opacity-30 px-5 py-3 transition hover:scale-105">{techItem.trim()}</div>)
-                }
-              </div>
-            </div>
-          </div>
-        </div>
+          )
+        }
       </div>
     </div>
   )
--- app/routes/projects/???/index.tsx
+++ app/routes/projects/???/index.tsx
@@ -3,8 +3,8 @@ export default function Unknown() {
     <div className="w-screen h-screen bg-black">
       <div className="flex flex-col mx-auto container w-full h-full align-center content-center justify-center items-center ">
         <div className="flex justify-center flex-col text-center align-center">
-          <h1 className="text-[200px] leading-[200px] font-['bely-display'] w-full h-full justify-center transition opacity-40 text-white">???</h1>
-          <h2 className="text-[100px] w-full h-full justify-center">Coming soon.</h2>
+          <h1 className="text-[200px] leading-[200px] max-xl:text-[48px] max-xl:leading-[48px] font-['bely-display'] w-full h-full justify-center transition opacity-40 text-white">???</h1>
+          <h2 className="text-[72px] max-xl:text-[32px] w-full h-full justify-center">Coming soon.</h2>
         </div>
       </div>
     </div>
