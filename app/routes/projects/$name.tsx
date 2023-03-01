c2ee9595c009c32b09fda692aac761662bf53b01
connergillette <connerwgillette@gmail.com>
2023-02-28 20:21:27 -0800

    Tweak project page

--- app/routes/projects/$name.tsx
+++ app/routes/projects/$name.tsx
@@ -54,27 +54,24 @@ export default function ProjectName () {
                     )
                   }
                     
-                  <div className="flex flex-col font-['bely-display'] relative bg-white rounded-md max-xl:w-full w-4/12 min-h-[400px]">
-                    <div className="p-5">
-                      <h2 className="text-2xl mb-3">Updates</h2>
-                      <div className="flex flex-col grow gap-3 scroll-smooth w-full h-full overflow-y-auto rounded-md max-h-[400px] pb-[100px]">
-                        {
-                          updates.map((update: Update) => update && <UpdateOverview key={update.id} slug={`/projects/${name}/${update.slug}`} update={update} /> )
-                        }
-                        {
-                          updates.map((update: Update) => update && <UpdateOverview key={update.id} slug={`/projects/${name}/${update.slug}`} update={update} /> )
-                        }
-                        {
-                          updates.map((update: Update) => update && <UpdateOverview key={update.id} slug={`/projects/${name}/${update.slug}`} update={update} /> )
-                        }
+                    <div className="flex flex-col font-['bely-display'] relative bg-white rounded-md max-xl:w-full w-4/12 min-h-[400px]">
+                      <div className="p-5">
+                        <h2 className="text-2xl mb-3">Updates</h2>
+                        <div className="flex flex-col grow gap-3 scroll-smooth w-full h-full overflow-y-auto rounded-md max-h-[400px] pb-[100px]">
+                          {
+                            updates.length > 0 && updates.map((update: Update) => update && <UpdateOverview key={update.id} slug={`/projects/${name}/${update.slug}`} update={update} /> )
+                          }
+                          {
+                            updates.length === 0 && (<span className="text-center w-full font-['bely']">None right now.</span>)
+                          }
+                        </div>
                       </div>
+                      {/* TODO: Potentially unused */}
+                      {/* <div className="flex justify-center align-center">
+                        <button className="button bg-[#495993] text-white w-min p-2 mt-2 rounded-md">More</button>
+                      </div> */}
+                      <div className="h-32 w-full absolute bottom-0 bg-gradient-to-t from-white to-transparent select-none pointer-events-none rounded-md"></div>
                     </div>
-                    {/* TODO: Potentially unused */}
-                    {/* <div className="flex justify-center align-center">
-                      <button className="button bg-[#495993] text-white w-min p-2 mt-2 rounded-md">More</button>
-                    </div> */}
-                    <div className="h-32 w-full absolute bottom-0 bg-gradient-to-t from-white to-transparent select-none pointer-events-none rounded-md"></div>
-                  </div>
                 </div>
                 <div className="text-white w-8/12 max-xl:w-full">
                   { project.details && 
