fe6847ea5196d44439ad01a5fbe6a064eca88792
connergillette <connerwgillette@gmail.com>
2023-06-05 01:41:58 -0700

    Add link Pills to header, link stack items

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -11,21 +11,23 @@ interface Props {
 export default function ListItem ({ children, color }: Props) {
 
   return (
-    <div className={`flex flex-col gap-4 w-full rounded-lg p-2 ${color ? `${color.border} ${color.bg} border-solid border-2` : ``}`}>
-      <div className="flex">
-        <div className="uppercase py-1 font-bold grow">Title of item</div>
-        <Pill text={'06/01/2023'} />
-      </div>
-      <div className="flex gap-4">
-        <div className="w-3/4">
-          {children}
+    <a href="/slug">
+      <div className={`flex flex-col gap-4 w-full rounded-lg p-2 ${color ? `${color.border} ${color.bg} border-solid border-2` : ``}`}>
+        <div className="flex">
+          <div className="uppercase py-1 font-bold grow">Title of item</div>
+          <Pill text={'06/01/2023'} />
         </div>
-        <ImageBlock 
-          src="https://media.licdn.com/dms/image/D5635AQEUr6E1M5kJOA/profile-framedphoto-shrink_400_400/0/1671171070141?e=1686301200&v=beta&t=Qt7TjIBotCHFPGixDrU1nuKozIqTmtm0dqsceQp8GyU" 
-          alt="Man with dark hair and glasses"
-          color={color}
-        />
+        <div className="flex gap-4">
+          <div className="w-3/4">
+            {children}
+          </div>
+          <ImageBlock 
+            src="https://media.licdn.com/dms/image/D5635AQEUr6E1M5kJOA/profile-framedphoto-shrink_400_400/0/1671171070141?e=1686301200&v=beta&t=Qt7TjIBotCHFPGixDrU1nuKozIqTmtm0dqsceQp8GyU" 
+            alt="Man with dark hair and glasses"
+            color={color}
+          />
+        </div>
       </div>
-    </div>
+    </a>
   )
 }
--- app/components/MainHeader.tsx
+++ app/components/MainHeader.tsx
@@ -1,14 +1,20 @@
+import Pill from './Pill'
+
 interface Props {
   text: string
 }
 
 export default function MainHeader (props: Props) {
   return (
-    <div className="max-w-[800px] m-5 ml-3">
-      <a href="/">
-        <h1 {...props} className="text-2xl font-bold">{props.text}</h1>
-      </a>
-      <h2 className="text-xl italic  opacity-40">Full-stack software engineer based in Redmond, WA, USA.</h2>
+    <div className="flex max-w-[800px] m-5 ml-3 grow gap-4">
+      <div className="flex flex-col grow">
+        <a href="/">
+          <h1 {...props} className="text-2xl font-bold">{props.text}</h1>
+        </a>
+        <h2 className="text-xl italic  opacity-40">Full-stack software engineer based in Redmond, WA, USA.</h2>
+      </div>
+      <a href="https://linkedin.com/in/connergillette"><Pill text="LinkedIn" /></a>
+      <a href="https://github.com/connergillette"><Pill text="GitHub" /></a>
     </div>
   )
 }
