624e62dccf2ab03c5e486b97f5e35b20df6fd510
connergillette <connerwgillette@gmail.com>
2023-06-07 19:28:27 -0700

    Add Supabase client, set up event fetching

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,24 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { useLoaderData } from 'react-router'
 import { themeColors } from '~/colors'
 import ImageGrid from '~/components/ImageGrid'
 import ListItem from '~/components/ListItem'
 import SectionHeader from '~/components/SectionHeader'
+
+import { supabase } from '~/supabase'
+
+const activities = ['professional', 'side_projects', 'hobbies']
+
+interface Event {
+  id: number,
+  created_at: Date,
+  data_content: string,
+  name: string,
+  image_url: string,
+  body: string,
+  pill_contents: string[],
+  activity_type: string,
+}
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -11,7 +27,27 @@ export const meta: V2_MetaFunction = () => {
   ]
 }
 
+export async function loader () {
+  const eventsRequest = await supabase.from('events').select()
+  let events : any = {}
+  if (eventsRequest.error) {
+    return { error: eventsRequest.error }
+  } else {
+    for (const event of eventsRequest.data) {
+      if (event.activity_type in Object.keys(event)) {
+        events[event.activity_type].push(event)
+      } else {
+        events[event.activity_type] = [event]
+      }
+    }
+  }
+
+  return { events }
+}
+
 export default function Index() {
+  const { events } = useLoaderData()
+
   return (
     <div className="flex flex-col gap-10 mb-10">
       <div className="grid grid-flow-row grid-cols-2 max-md:grid-cols-1 gap-10">
