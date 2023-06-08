6cd62848c0953f6f07c62f74a822adb95e5deec4
connergillette <connerwgillette@gmail.com>
2023-06-08 02:10:22 -0700

    Set up detail page, adjust Statuses

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -27,7 +27,7 @@ interface Props {
 
 export default function ListItem ({ event, children, color, pills, dateContent, compact, isFirst }: Props) {
   return (
-    <a href="/slug">
+    <a href={`/${event.id}`}>
       {
         !compact && (
           <div className={`flex flex-col gap-2 min-h-[170px] w-full max-w-full rounded-lg p-2 ${color ? `${color.border} ${color.bg} border-solid border-2` : ``}`}>
@@ -64,7 +64,9 @@ export default function ListItem ({ event, children, color, pills, dateContent, 
           <div className={`flex gap-2 w-full rounded-lg p-2 ${!isFirst ? 'px-5' : ''} ${color ? `${color.border} ${color.bg} border-solid border-2` : ``}`}>
             <div className="flex flex-col w-full">
               <div className="uppercase py-1 font-bold grow">{event.name}</div>
-              <Pill text={event.date_content} />
+              <div className="flex gap-2">
+                <Pill text={event.date_content} />
+              </div>
             </div>
             <div className="flex gap-4 grow">
               <div className="w-16 h-16">
--- app/components/Status.tsx
+++ app/components/Status.tsx
@@ -4,8 +4,9 @@ interface Props {
 
 export default function Status ({ text }: Props) {
   return (
-    <div className="rounded-full bg-slate-100 w-min px-4 py-1 whitespace-nowrap uppercase text-center text-xs">
-      {text}
+    <div className="flex gap-2 rounded-full bg-slate-100 w-min py-1 pl-2 pr-4 whitespace-nowrap uppercase text-center text-xs align-middle self-center">
+      <div className="bg-green-400 w-3 h-3 rounded-full self-center"></div>
+      <div>{text}</div>
     </div>
   )
 }
