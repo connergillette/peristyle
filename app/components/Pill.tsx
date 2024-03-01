5cab0e0d217e3f86b2d818f73036aefe4c642c4b
connergillette <connerwgillette@gmail.com>
2024-02-29 16:22:17 -0800

    Update pill outline weight

--- app/components/Pill.tsx
+++ app/components/Pill.tsx
@@ -12,6 +12,6 @@ export default function Pill ({ text, contrast, isButton, bgColor }: Props) {
   // const styles = outlineColor + ' ' + textColor
 
   return (
-    <div className={`rounded-lg py-1 px-3 ${isButton ? 'text-md font-bold' : 'text-sm'} uppercase h-min w-min whitespace-nowrap ${contrast ? 'bg-gray-100 text-gray-800' : (bgColor ? 'outline' : 'bg-green-400 text-gray-800 font-bold')}`}>{text}</div>
+    <div className={`rounded-lg py-1 px-3 ${isButton ? 'text-md font-bold' : 'text-sm'} uppercase h-min w-min whitespace-nowrap ${contrast ? 'bg-gray-100 text-gray-800' : (bgColor ? 'outline outline-1' : 'bg-green-400 text-gray-800 font-bold')}`}>{text}</div>
   )
 }
